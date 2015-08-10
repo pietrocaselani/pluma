7f300478e9bf70c8e1bf2428e798f35ffc8513b5
Pietro Caselani <pietro.caselani@fullsoft.com.br>
2015-08-10 14:05:54 -0300

    Add gradle

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-rm -rf obj/ libs/
-ndk-build clean
-ndk-build
+rm -rf src/main/libs
+rm -rf src/main/obj
+
+ndk-build NDK_PROJECT_PATH=src/main clean
+ndk-build NDK_PROJECT_PATH=src/main
 
 PLATFORM='unknown'
 if [[ "$(uname)" == 'Darwin' ]]; then
@@ -11,8 +13,10 @@ else
    PLATFORM='unix'
 fi
 
-mkdir -p libs/$PLATFORM
-cd libs/$PLATFORM
-cmake ../../
-cd ../../
-cmake --build libs/$PLATFORM --target all
+mkdir -p build/libs/$PLATFORM
+cd build/libs/$PLATFORM
+cmake ../../../
+cd ../../../
+cmake --build build/libs/$PLATFORM --target all
+mkdir -p src/main/libs/$PLATFORM
+cp -irfv build/libs/$PLATFORM/libpluma.* src/main/libs/
